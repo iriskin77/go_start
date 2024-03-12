86785a595f25c7add537836cd6d4ab91fba0f1e2
Me <vlad34b8@gmail.com>
2024-03-12 09:28:50 +0500

    connection to postgres added

--- cmd/main.go
+++ cmd/main.go
@@ -7,18 +7,45 @@ import (
 	"github.com/iriskin77/go_start/pkg/handler"
 	"github.com/iriskin77/go_start/pkg/repository"
 	"github.com/iriskin77/go_start/pkg/service"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 )
 
 func main() {
+
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initialization config %s", err.Error())
+	}
+
+	// Инициализируем БД
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: viper.GetString("db.password"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+
+	if err != nil {
+		log.Fatal("failed to initialize db: %s", err.Error())
+	}
+
 	// инициализируем экземпляр сервера с помощью ключевого слова new(). Благодаря New мы передаем указатель на сервер
 
-	repos := repository.NewRepository()
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	server := new(gostart.Server)
 
-	if err := server.Run("9090", handlers.InitRoutes()); err != nil {
+	if err := server.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while ruuning http server %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("/home/abc/Рабочий стол/go_start/configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
